f043fabe07be1fa885826484bb0aedec79d7f492
mtics <computer.science@foxmail.com>
2020-04-16 08:39:48 +0800

    try to fix nn.linear

--- libs/model.py
+++ libs/model.py
@@ -231,7 +231,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(inplace=True)
         )
 
-        self.fc = nn.Linear(1, 1)
+        self.fc = nn.Linear(128, 1)
 
     def forward(self, x):
         # input 512x512x3 to output 512x512x16
