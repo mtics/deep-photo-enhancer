ba72a5b4261ea7db1b19e4c70091d8a91704f4db
mtics <computer.science@foxmail.com>
2020-04-24 20:10:08 +0800

    dataparallel before load_state_dict

--- 2WayGAN_Train.py
+++ 2WayGAN_Train.py
@@ -22,20 +22,20 @@ if __name__ == "__main__":
 
     # Creating generator and discriminator
     generator_xy = Generator()
-    generator_xy.load_state_dict(torch.load('./gan2_train_299_xy.pth'))
     generator_xy = nn.DataParallel(generator_xy)
+    generator_xy.load_state_dict(torch.load('./gan2_train_299_xy.pth'))
 
     generator_yx = Generator()
-    generator_yx.load_state_dict(torch.load('./gan2_train_299_yx.pth'))
     generator_yx = nn.DataParallel(generator_yx)
+    generator_yx.load_state_dict(torch.load('./gan2_train_299_yx.pth'))
 
     discriminator_x = Discriminator()
-    discriminator_x.load_state_dict(torch.load('./discriminator2_train_299_xy.pth'))
     discriminator_x = nn.DataParallel(discriminator_x)
+    discriminator_x.load_state_dict(torch.load('./discriminator2_train_299_xy.pth'))
 
     discriminator_y = Discriminator()
-    discriminator_y.load_state_dict(torch.load('./discriminator2_train_299_yx.pth'))
     discriminator_y = nn.DataParallel(discriminator_y)
+    discriminator_y.load_state_dict(torch.load('./discriminator2_train_299_yx.pth'))
 
     if torch.cuda.is_available():
         generator_xy.cuda(device=device)
